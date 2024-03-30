f1cce9f005700464ed9cef977962c4c7e5144565
mariajuliacarvalhoo14 <~161620231069@faculdadedamas.edu.br>
2024-03-30 16:27:11 -0300

    versao atualizada com informações da aula

---
+++ usando_funcao_range.py
@@ -0,0 +1,4 @@
+#exibindo tabuada do 5
+for numero in range (0,51,5):
+    print (numero, end= " ") # o end serve para o resultado do console não ser expressado um abaixo do outro e sim um do lado do outro
+    
